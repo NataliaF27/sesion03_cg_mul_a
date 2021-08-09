19e8bca4936e64fb8faa11ddabe14754fbb34290
Natalia Fuquene <est.natalia.fuquene@unimilitar.edu.co>
2021-08-09 10:49:43 -0500

    Natalia Fuquene <est.natalia.fuquene@unimilitar.edu.co>

--- sesion03_cg_mul_a/src/main/java/Esferica.java
+++ sesion03_cg_mul_a/src/main/java/Esferica.java
@@ -4,18 +4,29 @@ public class Esferica {
     float y; // angulo con respecto al eje x de la proyeccion de punto sobre el plano x-y
     float z; //angulo del vector formado por el punto y el origen con respecto al eje z
 
-    public Cilindrica cilindrica_esferica(float x,float y,float z){
+    public Cilindrica esferica_cilindrica(float x,float y,float z){
         float radio=(float) (x*Math.sin(y)); 
         float angulo=z; 
         float zeta=(float)(x*Math.cos(y)); 
         return new Cilindrica(radio,angulo,zeta); 
+       
     }
     
-    public Esferica esferica_cilindrica(float angulo,float radio, float zeta){
+     public Cilindrica esferica_cilindrica (Cilindrica c){
+       
+        return esferica_cilindrica (c.getRadio(),c.getAngulo(),c.zeta); 
+       
+    }
+    public Esferica cilindrica_esferica(float angulo,float radio, float zeta){
         float x=(float)(Math.sqrt((Math.pow(radio,2)+(Math.pow(zeta,2))))); 
         float y=(float)(Math.atan(radio/zeta)); 
         float z=zeta; 
         return new Esferica (x,y,z); 
+    }
+     public  Esferica cilindrica_esferica (Esferica e){
+       
+        return cilindrica_esferica (e.getX(),e.getY(),e.getZ()); 
+       
     }
     public Esferica() {
     }
